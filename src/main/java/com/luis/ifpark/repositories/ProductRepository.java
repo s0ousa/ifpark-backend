d9492c44a909775caa564d10548e32fa5a9514c6
Luis Sousa <las.sousa@outlook.com>
2025-12-20 21:41:08 -0300

    removing product entitie

--- src/main/java/com/luis/ifpark/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luis.ifpark.repositories;
-
-import com.luis.ifpark.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
